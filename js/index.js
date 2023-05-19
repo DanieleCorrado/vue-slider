8138fe41882013de18537dda80758cd86bbfdbef
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-19 16:23:54 +0200

    add vue slider

--- js/index.js
+++ js/index.js
@@ -1,74 +1,63 @@
-const images = [
-  {
-      'image': 'img/01.webp',
-      'title': "Marvel\\'s Spiderman Miles Morale",
-      'text': 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-  }, {
-      'image': 'img/02.webp',
-      'title': 'Ratchet & Clank: Rift Apart',
-      'text': 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-  }, {
-      'image': 'img/03.webp',
-      'title': 'Fortnite',
-      'text': "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  }, {
-      'image': 'img/04.webp',
-      'title': 'Stray',
-      'text': 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-  }, {
-      'image': 'img/05.webp',
-      'title': "Marvel's Avengers",
-      'text': "Marvel\\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  }
-];
+const { createApp } = Vue;
 
-let slideIndex = 1;
-showSlides(slideIndex);
+createApp({
 
-// Controlli immagini precedente e successivo
+  data() {
+    return {
+      activeImage: 0,
+      images: [
+        {
+            'image': 'img/01.webp',
+            'title': "Marvel\\'s Spiderman Miles Morale",
+            'text': 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        }, {
+            'image': 'img/02.webp',
+            'title': 'Ratchet & Clank: Rift Apart',
+            'text': 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        }, {
+            'image': 'img/03.webp',
+            'title': 'Fortnite',
+            'text': "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        }, {
+            'image': 'img/04.webp',
+            'title': 'Stray',
+            'text': 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+        }, {
+            'image': 'img/05.webp',
+            'title': "Marvel's Avengers",
+            'text': "Marvel\\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        }
+      ]
+    }
+  },
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  methods: {
 
-// Controlli thumbnail
+    nextImage() {
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+      if (this.activeImage === this.images.length - 1) {
 
-function showSlides(n) {
-  let i;
+        this.activeImage = 0;
+       
+      } else {
 
-  let slides = document.getElementsByClassName("mySlides");
+        this.activeImage++;
 
-  let dots = document.getElementsByClassName("demo");
+      }
+    },
 
-  let title = document.getElementById("title");
+    preImage() {
+      if (this.activeImage === 0) {
 
-  let description = document.getElementById("description");
+        this.activeImage = this.images.length - 1;
 
-  if (n > slides.length) {
-    slideIndex = 1
+      } else {
 
-  }
+        this.activeImage--;
 
-  if (n < 1) {
-    slideIndex = slides.length
-  }
+      }
+    }
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  slides[slideIndex-1].style.display = "block";
-
-  dots[slideIndex-1].className += " active";
-  
-  title.innerHTML = dots[slideIndex-1].alt;
-  description.innerHTML = images[slideIndex-1].text;
-}
+}).mount("#app")
